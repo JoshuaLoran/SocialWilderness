5dbb39a656b58f67bdb75121035b995d74ae17bb
Joshua Loran <joshua.loran@gmail.com>
2019-03-25 16:28:24 -0700

    added user email and pw and fix image show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_163903) do
+ActiveRecord::Schema.define(version: 2019_03_25_231045) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "booking_url"
@@ -19,16 +19,6 @@ ActiveRecord::Schema.define(version: 2019_03_25_163903) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -55,6 +45,8 @@ ActiveRecord::Schema.define(version: 2019_03_25_163903) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
 end
