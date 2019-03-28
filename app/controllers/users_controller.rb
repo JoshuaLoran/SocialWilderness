219526eb299d3a627807c1d8741048646d14d97d
Kimberly Heidorn <heidorkn@gmail.com>
2019-03-28 14:34:02 -0700

    making things look pretty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,33 @@ class UsersController < ApplicationController
   before_action :set_users, only: [:edit, :update, :destroy, :profile, :show]
   skip_before_action :require_login, only: [:show, :index, :search, :new, :create]
 
-   def index
-     @users = User.all
-   end
+  def index
+    @users = User.all
+  end
 
-   def new
-     @user = User.new
-   end
+  def new
+    @user = User.new
+  end
 
-   def edit
-     if current_user.id == @user.id
-       render :edit
-     else
-       flash[:message] = 'Unauthorized Action'
-       redirect_to "/users/#{@user.id}/profile"
-     end
-   end
+  def edit
+    if current_user.id == @user.id
+      render :edit
+    else
+      flash[:message] = 'Unauthorized Action'
+      redirect_to "/users/#{@user.id}/profile"
+    end
+  end
 
-   def search
-     @user = User.search(params[:search])
-   end
+  def search
+    @user = User.search(params[:search])
+  end
 
-   def profile
-     @relationship = current_user.active_relationships.where(followed_id: @user.id).first
-     render :profile
-   end
+  def profile
+    @relationship = current_user.active_relationships.where(followed_id: @user.id).first
+    render :profile
+  end
 
-   def create
+  def create
     user=User.create(user_params)
     if user.valid?
       session[:user_id] = user.id
@@ -37,28 +37,27 @@ class UsersController < ApplicationController
       flash[:errors] = user.errors.full_messages
       redirect_to new_user_path
     end
-   end
+  end
 
-   def update
-     @user.update(user_params)
-     redirect_to @user
-   end
+  def update
+    @user.update(user_params)
+    redirect_to @user
+  end
 
-   def destroy
-     @user.destroy
-     session.clear
-     redirect_to '/login'
-   end
+  def destroy
+    @user.destroy
+    session.clear
+    redirect_to '/login'
+  end
 
-   private
+  private
 
-   def set_users
-     @user = User.find(params[:id])
-   end
+  def set_users
+    @user = User.find(params[:id])
+  end
 
-   def user_params
-     params.require(:user).permit!
-   end
-
+  def user_params
+    params.require(:user).permit!
+  end
 
 end
