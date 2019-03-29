4ddb3ae5acd2dccb1adcbabdc38f1746f2f827cf
Joshua Loran <joshua.loran@gmail.com>
2019-03-29 09:54:03 -0700

    added some seeds

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_27_195631) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "destinations", force: :cascade do |t|
     t.string "booking_url"
     t.string "name"
